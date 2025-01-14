0aa55661af992934a664feb5f56848566debbf03
AndriiTyschuk <andrii.tyschuk@ithoot.com>
2025-01-14 14:21:56 +0200

    add startup page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartpageComponent } from './modules/startpage/startpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartpageComponent
   ],
   imports: [
     BrowserModule,
